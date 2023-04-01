da4f04c44327a9b513c1ad9996269acdd99ece29
HitsujiRere <hitsujirere@gmail.com>
2023-04-01 20:31:59 +0900

    PIC&BAN画面下部に言語選択ボタンを追加

--- extension/script.js
+++ extension/script.js
@@ -3,9 +3,30 @@
 
   if (urlSearchParams.has('draft')) {
     if (urlSearchParams.get('locale') === 'ja_JP') {
+      // Script読み込み
       const scriptElement = document.createElement('script');
       scriptElement.src = chrome.runtime.getURL('prodraft.js');
       document.body.appendChild(scriptElement);
+
+      // 英語選択ボタン
+      const switchEnUsElement = document.createElement('button');
+      switchEnUsElement.addEventListener('click', () => {
+        urlSearchParams.set('locale', 'en_US');
+        window.location.search = urlSearchParams;
+      });
+      switchEnUsElement.innerText = 'English';
+      switchEnUsElement.classList.add('button');
+      document.body.appendChild(switchEnUsElement);
+    } else {
+      // 日本語選択ボタン
+      const switchJaJpElement = document.createElement('button');
+      switchJaJpElement.addEventListener('click', () => {
+        urlSearchParams.set('locale', 'ja_JP');
+        window.location.search = urlSearchParams;
+      });
+      switchJaJpElement.innerText = '日本語';
+      switchJaJpElement.classList.add('button');
+      document.body.appendChild(switchJaJpElement);
     }
   }
 })();
